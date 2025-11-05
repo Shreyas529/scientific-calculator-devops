87a1ec16bed0954323997d89501f7aa196ab613d
Shreyas <Shreyas.Saggere@iiitb.ac.in>
2025-11-05 20:17:35 +0530

    Added comment to test the webhook

--- calculator.py
+++ calculator.py
@@ -52,11 +52,5 @@ def menu():
 
 if __name__ == "__main__":
     # Comment to test the webhook
-    # Comment 2
-    # Comment 3
-    # Comment 4
-    # Comment 5
-    # Comment 6
-    # Comment 7
     menu()
 
